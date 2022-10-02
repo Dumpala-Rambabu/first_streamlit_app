886579da643ddb0a2da47c5c69d562bc634459df
Dumpala Rambabu <114546728+Dumpala-Rambabu@users.noreply.github.com>
2022-10-02 08:36:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit
+streamlit.title('My Mom's New Heathly Diner')
 
-streamlit.title('My Parents New Heathly Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale,spinch & Rocket Smoothie')
 streamlit.text('Hard-Blood Free-Range Egg')
 
             
-streamlit.title('My Mom's New Heathly Diner')
              
