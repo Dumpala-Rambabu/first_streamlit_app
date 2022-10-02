29ab7e6258661b95caa09515fe1fb938f063f9f3
Dumpala Rambabu <114546728+Dumpala-Rambabu@users.noreply.github.com>
2022-10-02 09:06:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-         
+ 
+         import streamlit
+
        streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
       
